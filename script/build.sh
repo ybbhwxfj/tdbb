827413d68fb1b474e3af67000a9f24504d689bca
ybbh <fchdir@qq.com>
2021-12-28 10:48:10 +0800

    tarvis

---
+++ script/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dir=$(dirname "$0")
+proj_dir=$(cd "$dir" || exit; cd ..; pwd)
+mkdir -p "${proj_dir}/build"
+cd "${proj_dir}/build" || exit;
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j
